9dd04fc99bb700edf4fdf13df708ca2c45a4e776
quanhn4002 <quannhph36246@fpt.edu.vn>
2024-01-06 16:25:25 +0700

    add

---
+++ ONTAP_1/main.js
@@ -0,0 +1,61 @@
+// Log number
+console.log(100);
+
+// Log string
+console.log('Hello World');
+
+// Log multiple values
+console.log(20, 'Hello', true);
+
+// Log a variable
+const x = 100;
+console.log(x);
+
+// Console error & warning
+console.error('Alert');
+console.warn('Warning');
+
+// Log object as table
+console.table({ name: 'Brad', email: 'brad@gmail.com' });
+
+// Group console commands
+console.group('simple');
+console.log(x);
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd();
+
+// Add CSS to logs
+const styles = 'padding: 10px; background-color: white; color: green';
+console.log('%cHello World', styles);
+
+// Variable substitution
+let score;
+score = 1;
+console.log(score);
+
+if (true) {
+  score = score + 1;
+}
+
+console.log(score);
+const arr = [1, 2, 3, 4];
+arr.push(5);
+console.log(arr);
+
+const person = {
+  name: 'Brad',
+};
+person.name = 'John';
+person.email = 'brad@gmail.com';
+console.log(person);
+
+// Declare multiple values at once
+let a, b, c;
+
+const d = 10,
+  e = 20,
+  f = 30;
+
+console.log(d);
+console.log(a);
